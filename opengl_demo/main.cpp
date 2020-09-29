a81298a7bc7750b3195654a18befba5c8c421b3c
CregskiN <272473132@qq.com>
2020-09-29 13:27:10 +0800

    opengl: finish texture.

--- opengl_demo/main.cpp
+++ opengl_demo/main.cpp
@@ -5,9 +5,8 @@
 #include <sstream>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-//#include "../static/scvtb_image.h"
 
-#include "shader_s.h" // 强制使用绝对路径
+#include </Users/cregskin/code/c/opengl_demo/opengl_demo/shader_s.h> // 强制使用绝对路径
 
 const unsigned int SCR_WIDTH = 1000;
 const unsigned int SCR_HEIGHT = 650;
@@ -43,6 +42,43 @@ int main(){
     
     Shader ourShader("/Users/cregskin/code/c/opengl_demo/opengl_demo/shader.vs", "/Users/cregskin/code/c/opengl_demo/opengl_demo/shader.fs");
     
+    
+    // 定义 3D顶点坐标、颜色RGB、2D纹理坐标
+    float vertices[] = {
+        //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // 第一个三角形：右上、右下、左下
+        1, 2, 3  // 第二个三角形：右下、左下、左上
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    // 3D位置属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // 顶点位置值 顶点大小 数据类型 是否标准化 步长 数据在缓冲起始位置
+    glEnableVertexAttribArray(0); // 顶点属性位置值为参数，启用顶点属性
+    // 颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // 纹理属性
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
+    
     unsigned int texture;
     glGenTextures(1, &texture); // 定义纹理数据空间
     glBindTexture(GL_TEXTURE_2D, texture); // 绑定纹理空间
@@ -53,43 +89,17 @@ int main(){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // 纹理放大，使用 linear 过滤纹理
     
     int width, height, nrChannels;
-    
-    unsigned char* data = stbi_load("../static/container.png",&width,&height,&nrChannels, 0); // filename width height numberOfChannels
+    std::string inputPath = "/Users/cregskin/code/c/opengl_demo/opengl_demo/container.jpg";
+    unsigned char* data = stbi_load(inputPath.c_str(),&width,&height,&nrChannels, 0); // filename width height numberOfChannels
     if(data){
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data); // 纹理目标(处于opengl) 多级渐远纹理级别0为基本级别 纹理储存的格式 固定0历史遗留问题 原图格式和数据类型 原数据
+        // 纹理目标(处于opengl) 多级渐远纹理级别0为基本级别 纹理储存的格式 固定0历史遗留问题 原图格式和数据类型 原数据
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }else {
         std::cout << "Error: Faile to load image." << std::endl;
     }
     stbi_image_free(data); // 释放 data 数据
     
-    // 定义 3D顶点坐标、颜色RGB、2D纹理坐标
-    float vertices[] = {
-        //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
-    };
-    //    toNDC(vertices, sizeof(vertices), SCR_WIDTH, SCR_HEIGHT); // 标准化设备坐标
-    
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    // 3D位置属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // 顶点位置值 顶点大小 数据类型 是否标准化 步长 数据在缓冲起始位置
-    glEnableVertexAttribArray(0); // 顶点属性位置值为参数，启用顶点属性
-    // 颜色属性
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // 纹理属性
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
-    glEnableVertexAttribArray(2);
     
     while(!glfwWindowShouldClose(window)){
         processInput(window);
@@ -101,7 +111,7 @@ int main(){
         
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_POINTS, 0, 2); // 数据起始位置 点数
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // 绘制模式 绘制顶点个数 索引类型 EBO中的偏移量
         
         glfwSwapBuffers(window);
         glfwPollEvents();
