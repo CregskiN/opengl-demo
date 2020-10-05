199b9b1554d0efeb2491472b73bb497bef41bf0c
CregskiN <272473132@qq.com>
2020-10-05 09:19:59 +0800

    fix: the 'awesomeface.png' has a channel, when i use getTexImage2D, i must set the seventh param to GL_RGBA

---
+++ note/入门/7. 变换/正文/lib的副本.cpp
@@ -0,0 +1,12 @@
+//
+//  lib.cpp
+//  opengl_demo
+//
+//  Created by 吴昊 on 2020/9/27.
+//  Copyright © 2020 吴昊. All rights reserved.
+//
+
+//#define STB_IMAGE_IMPLIEMENTATION // 预处理器会让
+//#include <stb_image.h>
+//#include </Users/cregskin/code/c/opengl_demo/opengl_demo/stb_image.h>
+
